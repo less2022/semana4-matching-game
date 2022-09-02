4e4c6bb96fb0a22c418a154d6eba9b3259398345
Lesly Carolina Bermudez Membreño <USSS017722@ugb.edu.sv>
2022-09-02 09:54:18 -0600

    primer psrte del codigo.

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,63 @@ using System.Windows.Forms;
 
 namespace semana4_primer_proyecto_formulario
 {
-   
+    public partial class Form1 : Form
+
+
+
+
+    {
+       
+        Label firstClicked = null;
+
+      
+
+        Label secondClicked = null;
+
+
+        Random random = new Random();
+
+        // cada una de las letras es un icono interesante 
+        // y cada icono aparece dos veces en esta lista
+
+
+        List<string> icons = new List<string>()
+    {
+        "A", "A", "O", "O", "C", "C", "Ñ", "Ñ",
+        "P", "P", "Q", "Q", "X", "X", "T", "T"
+    };
+
+        /// <resumen>
+        /// Asigna cada icono de la lista de iconos a un cuadrado alaeatorio
+        /// </resumen>
+
+
+        private void AssignIconsToSquares()
+        {
+            // El TableLayoutPanel tiene 16 etiquetas,
+            // Y la lista de iconos tiene 16 iconos,
+            // Por lo que se extrae un icono al azar de la lista
+            // y añadido a cada etiqueta
+
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+
+
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+
+                }
+            }
+        }
+
+
+      
         }
 
 
